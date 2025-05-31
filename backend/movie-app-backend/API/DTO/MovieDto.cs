b5bcb053c97253b05f8d4dd8e1196b422013f85a
lahiruomadhawa <lahirumadhawa00@gmail.com>
2025-06-01 07:03:11 +1000

    DTO and Services

--- backend/movie-app-backend/API/DTO/MovieDto.cs
+++ backend/movie-app-backend/API/DTO/MovieDto.cs
@@ -2,5 +2,29 @@
 {
     public class MovieDto
     {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Category { get; set; } = string.Empty;
+        public string Director { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Poster { get; set; } = string.Empty;
+    }
+
+    public class CreateMovieDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Category { get; set; } = string.Empty;
+        public string Director { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Poster { get; set; } = string.Empty;
+    }
+
+    public class UpdateMovieDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Category { get; set; } = string.Empty;
+        public string Director { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Poster { get; set; } = string.Empty;
     }
 }
